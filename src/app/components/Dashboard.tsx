70e95bb95cb040352189132afa1853e20ffa5ae5
seunnny <pse3048@gmail.com>
2026-06-16 17:34:28 +0900

    feat: 홈화면 여백 수정

--- src/app/components/Dashboard.tsx
+++ src/app/components/Dashboard.tsx
@@ -21,7 +21,7 @@ export default function Dashboard({ onNavigate, sessions }: Props) {
   ];
 
   return (
-    <div style={{ maxWidth: "1200px" }}>
+    <div style={{ width: "100%" }}>
       {/* Welcome */}
       <div style={{ marginBottom: "1.75rem" }}>
         <h1 style={{ color: "#111827", marginBottom: "0.25rem" }}>안녕하세요, 세은님 👋</h1>
